41bd51fd1731461635829ab608c28a2a0fa50c8d
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-08-06 12:06:17 +0530

    added netlify.toml file

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,5 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+        <App />
 )
